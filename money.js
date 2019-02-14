63c46af4bc157df3124962b3069d5a2b6322fa23
Hidetake Iwata <int128@gmail.com>
2019-02-14 09:18:41 +0900

    Move to native query selector and remove jQuery

--- money.js
+++ money.js
@@ -1,50 +1,78 @@
-window.addEventListener('load', function () {
-    var table = $('<table/>');
-    table.insertAfter($('#filter_payment_category_menu'));
-    table.toggle();
-    table.append($('<tr/>')
-        .append('<th>日付</th>')
-        .append('<th>大カテゴリ</th>')
-        .append('<th>小カテゴリ</th>')
-        .append('<th>金額</th>')
-        .append('<th>出金</th>')
-        .append('<th>入金</th>')
-        .append('<th>お店</th>')
-        .append('<th>品目</th>')
-        .append('<th>メモ</th>')
-    );
+'use strict';
 
-    $('tbody.money-list>tr').each(function () {
-        var date = parseZaimDateString($('td:nth-child(3)', this).text());
-        var category1st = $('td:nth-child(4)>span', this).attr('data-title');
-        var category2nd = $('td:nth-child(4)>a', this).text();
-        var price = $('td:nth-child(5)', this).text();
-        var fromAccount = $('td:nth-child(6) img', this).attr('alt');
-        var toAccount = $('td:nth-child(7) img', this).attr('alt');
-        var place = $('td:nth-child(8) span', this).attr('title');
-        var name = $('td:nth-child(9) span', this).attr('title');
-        var memo = $('td:nth-child(10) span', this).attr('title');
+window.addEventListener('load', () => {
+    var table = document.createElement('table');
+    table.classList.add('hide');
+    table.appendChild(createRowWithColumns('th', [
+        '日付',
+        '大カテゴリ',
+        '小カテゴリ',
+        '金額',
+        '出金',
+        '入金',
+        'お店',
+        '品目',
+        'メモ',
+    ]));
 
-        table.append($('<tr/>')
-            .append($('<td/>').text(date.toLocaleDateString()))
-            .append($('<td/>').text(category1st))
-            .append($('<td/>').text(category2nd))
-            .append($('<td/>').text(price))
-            .append($('<td/>').text(fromAccount))
-            .append($('<td/>').text(toAccount))
-            .append($('<td/>').text(place))
-            .append($('<td/>').text(name))
-            .append($('<td/>').text(memo))
-        );
+    document.querySelectorAll('tbody.money-list>tr').forEach(tr => {
+        var columns = [
+            applyOrEmpty(tr.querySelector('td:nth-child(3)'), e => parseZaimDateString(e.textContent).toLocaleDateString()),
+            applyOrEmpty(tr.querySelector('td:nth-child(4)>span'), e => e.getAttribute('data-title')),
+            applyOrEmpty(tr.querySelector('td:nth-child(4)>a'), e => e.textContent),
+            applyOrEmpty(tr.querySelector('td:nth-child(5)'), e => e.textContent),
+            applyOrEmpty(tr.querySelector('td:nth-child(6) img'), e => e.getAttribute('alt')),
+            applyOrEmpty(tr.querySelector('td:nth-child(7) img'), e => e.getAttribute('alt')),
+            applyOrEmpty(tr.querySelector('td:nth-child(8) span'), e => e.getAttribute('title')),
+            applyOrEmpty(tr.querySelector('td:nth-child(9) span'), e => e.getAttribute('title')),
+            applyOrEmpty(tr.querySelector('td:nth-child(10) span'), e => e.getAttribute('title')),
+        ];
+        table.appendChild(createRowWithColumns('td', columns));
     });
 
-    var button = $('<button class="btn btn-success">スプレッドシート形式</button>');
-    button.insertAfter($('#filter_payment_category_menu'));
-    button.on('click', function () {
-        table.toggle();
+    var toggle = document.createElement('button');
+    toggle.classList.add('btn', 'btn-success');
+    toggle.textContent = 'スプレッドシート形式';
+    toggle.addEventListener('click', () => {
+        table.classList.toggle('hide');
     });
+
+    var insertRef = document.getElementById('filter_payment_category_menu');
+    insertRef.parentNode.insertBefore(table, insertRef.nextElementSibling);
+    insertRef.parentNode.insertBefore(toggle, insertRef.nextElementSibling);
 });
 
+/**
+ * @param {Element} e 
+ * @param {(e: Element) => string} f 
+ * @returns {string}
+ */
+function applyOrEmpty(e, f) {
+    if (!(e instanceof Element)) {
+        return '';
+    }
+    return f(e);
+}
+
+/**
+ * @param {string} columnTagName 
+ * @param {Array<string>} columns 
+ * @returns {Element}
+ */
+function createRowWithColumns(columnTagName, columns) {
+    var tr = document.createElement('tr');
+    columns.forEach(column => {
+        var c = document.createElement(columnTagName);
+        c.textContent = column;
+        tr.appendChild(c);
+    });
+    return tr;
+}
+
+/**
+ * @param {string} s
+ * @returns {Date}
+ */
 function parseZaimDateString(s) {
     var y = new Date().getFullYear();
     var p = s.split(/[月日]/);
